f5f3835ecda46b1cfe209729b53b556ce9c266e6
Kevin Thompson <antiduh@gmail.com>
2018-06-11 22:54:36 -0400

    Started implementing DataConverter.
    Added support for WriteInt/ReadInt LE.

---
+++ Antiduh.ClassLib.Test/DataConverterTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Antiduh.ClassLib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Antiduh.ClassLib.Test
+{
+    [TestClass]
+    public class DataConverterTests
+    {
+        [TestMethod]
+        public void DataConverter_WriteIntLE_Rejects_InvalidBuffer()
+        {
+            byte[] small = new byte[3];
+            byte[] exact = new byte[4];
+            byte[] large = new byte[5];
+
+            Assert2.Throws<ArgumentException>( () => DataConverter.WriteIntLE( 0, small, 0 ) );
+            Assert2.Throws<ArgumentException>( () => DataConverter.WriteIntLE( 0, exact, 1 ) );
+            Assert2.Throws<ArgumentException>( () => DataConverter.WriteIntLE( 0, large, 2 ) );
+        }
+
+        [TestMethod]
+        public void DataConverter_ReadIntLE_Rejects_InvalidBuffer()
+        {
+            byte[] small = new byte[3];
+            byte[] exact = new byte[4];
+            byte[] large = new byte[5];
+
+            Assert2.Throws<ArgumentException>( () => DataConverter.ReadIntLE( small, 0 ) );
+            Assert2.Throws<ArgumentException>( () => DataConverter.ReadIntLE( exact, 1 ) );
+            Assert2.Throws<ArgumentException>( () => DataConverter.ReadIntLE( large, 2 ) );
+        }
+    }
+}
---
+++ Antiduh.ClassLib/DataConverter.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Antiduh.ClassLib
+{
+    /// <summary>
+    /// Converts common data types from their native type to their big-endian and little-endian
+    /// representations suitable for serialization.
+    /// </summary>
+    public static class DataConverter
+    {
+        /// <summary>
+        /// Writes a 32-bit signed integer to the given byte array in little-endian format. 4 bytes
+        /// are written to the array.
+        /// </summary>
+        /// <param name="value">The value to write to the byte array.</param>
+        /// <param name="data">The array to write to.</param>
+        /// <param name="start">The first index in the array to write to.</param>
+        public static void WriteIntLE( int value, byte[] data, int start )
+        {
+            if( data.Length - start < 4 )
+            {
+                throw new ArgumentException( "The provided array is not large enough to store the result." );
+            }
+
+            data[start + 0] = (byte)( value >> 0 );
+            data[start + 1] = (byte)( value >> 8 );
+            data[start + 2] = (byte)( value >> 16 );
+            data[start + 3] = (byte)( value >> 24 );
+        }
+
+        /// <summary>
+        /// Reads a 32-bit signed integer from a byte array that has the value stored in
+        /// little-endian format. 4 bytes are read from the array.
+        /// </summary>
+        /// <param name="data">The array to read from.</param>
+        /// <param name="start">The first index in the array to read from.</param>
+        public static int ReadIntLE( byte[] data, int start )
+        {
+            if( data.Length - start < 4 )
+            {
+                throw new ArgumentException( "The provided array is not large enough to read the value." );
+            }
+
+            int value = 0;
+
+            value = data[start + 0];
+            value |= data[start + 1] << 8;
+            value |= data[start + 2] << 16;
+            value |= data[start + 3] << 24; 
+
+            return value;
+        }
+
+        /// <summary>
+        /// Returns the bytes representing the value of the given 32-bit signed integer stored in
+        /// little-endian format. 4 bytes are returned.
+        /// </summary>
+        /// <param name="value">The value to convert to a little-endian bytes.</param>
+        /// <returns></returns>
+        public static byte[] GetIntBytesLE( int value )
+        {
+            byte[] data = new byte[4];
+
+            WriteIntLE( value, data, 0 );
+
+            return data;
+        }
+
+    }
+}

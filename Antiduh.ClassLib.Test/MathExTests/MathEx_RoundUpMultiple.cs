6ac029fc94ca4a4e468fd77947fe6d4c62c6b133
Kevin Thompson <antiduh@gmail.com>
2018-10-10 02:18:09 -0400

    New math helper class to compute round-up values and set bits.

---
+++ Antiduh.ClassLib.Test/MathExTests/MathEx_RoundUpMultiple.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Antiduh.ClassLib.Test.BitListTests
+{
+    [TestClass]
+    public class MathEx_RoundUpMultiple
+    {
+        [TestMethod]
+        public void MathEx_RoundUpMultiple_Valid()
+        {
+            // Maps input value to output values.
+            var testVectors = new Dictionary<int, int>()
+            {
+                {0, 0}, {1, 5}, {4, 5}, {5, 5}, {6, 10}, {10, 10}
+            };
+
+            foreach( var input in testVectors.Values )
+            {
+                int result = MathEx.RoundUpToMultiple( input, 5 );
+                int expected = testVectors[result];
+
+                Assert.AreEqual( expected, result );
+            }
+        }
+    }
+}
---
+++ Antiduh.ClassLib/MathEx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Antiduh.ClassLib
+{
+    public static class MathEx
+    {
+        public static int CountSetBits( int value )
+        {
+            // Thanks Kernighan.
+
+            int count = 0;
+
+            while( value > 0 )
+            {
+                value &= ( value - 1 );
+                count++;
+            }
+
+            return count;
+        }
+
+        public static int CountSetBits( long value )
+        {
+            int count = 0;
+
+            while( value > 0 )
+            {
+                value &= ( value - 1 );
+                count++;
+            }
+
+            return count;
+        }
+
+
+        public static int RoundUpToMultiple( int value, int multiple )
+        {
+            return ( ( value + 1 ) / multiple ) * multiple;
+        }
+    }
+}

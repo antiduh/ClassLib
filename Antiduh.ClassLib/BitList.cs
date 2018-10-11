ea95d4b47856e98049a950702b4a1f034577c24c
Kevin Thompson <antiduh@gmail.com>
2018-10-11 02:45:53 -0400

    Implemented growing and shrinking the list.

--- Antiduh.ClassLib/BitList.cs
+++ Antiduh.ClassLib/BitList.cs
@@ -17,8 +17,16 @@ namespace Antiduh.ClassLib
         /// </summary>
         private const int arrayBlockSize = 1024;
 
-        private int length;
+        /// <summary>
+        /// The number of logical bits stored in the list.
+        /// </summary>
+        private int bitLength;
 
+        /// <summary>
+        /// The buffer for storing set bits. Note, the buffer may have room for more bits than the
+        /// list logically contains. For instance, if bitLength = 5, then the buffer is one byte and
+        /// the top 3 bits are unused.
+        /// </summary>
         private byte[] data;
 
         public BitList()
@@ -33,7 +41,7 @@ namespace Antiduh.ClassLib
                 throw new ArgumentOutOfRangeException( nameof( initialLength ), initialLength, "Must be zero or greater." );
             }
 
-            this.length = initialLength;
+            this.bitLength = initialLength;
             this.data = new byte[BitsToBytes( initialLength )];
         }
 
@@ -56,7 +64,7 @@ namespace Antiduh.ClassLib
                 throw new ArgumentException( "The length of the array is too short to store the number of requested bits." );
             }
 
-            this.length = numBits;
+            this.bitLength = numBits;
             this.data = new byte[neededBytes];
 
             for( int i = 0; i < this.data.Length; i++ )
@@ -72,43 +80,97 @@ namespace Antiduh.ClassLib
         {
             get
             {
-                return this.length;
+                return this.bitLength;
             }
             set
             {
-                // If they're increasing the length of the bitlist, allocate more bytes. Easy.
+                // Increasing the bitlength of the list may not change the array length.
+                // - Increasing the bitlength by 1 may mean that we go from storing 2 bits to 3. No
+                // change at all in the number of bytes needed to handle that.
+                // - The array is allocated in large blocks (eg, 1024 bytes). If the bitlength of the
+                // list is only 3 bits, then 1023 bytes are past the logical end of the list.
                 //
-                // If they're decreasing the length of the BitList, it's a bit more complicated.
-                // - We might want to decrease the length of the array if they reduce the length by enough.
-                // - The last logical bit in the list might fall in the middle of the last remaining
-                //   byte, so we have to clear any set bits above the last logical index.
-                // - We're throwing away bits, so we have to count how many we're throwing away and
-                //   reduce `Count` appropriately.
+                // Unused bits are always zero. We clear bits when they become unused by any means.
+                //
+                // When we increase the bitlength of the list, we assume any newly used bytes are
+                // already zero and no clearing is necessary.
+                //
+                // When we decrease the bitlength of the list, we clear any bits that are past the
+                // logical end of the list. This includes bits in any completely unused bytes, as well
+                // as bits that might be in a partially-used byte.
+                //
+                // Increasing the size of the list:
+                // - Reallocate the array if we don't have enough bytes to store the requested amount.
+                // - Save the new bitlength of the list.
+                //
+                // Decreasing the size of the list:
+                // - Reallocate the array if we have enough bytes to give up a whole chunk.
+                // - Make sure any bits past the logical end of the list are zeroed.
 
+                /*
+                    Example. Let's say that our block size is 8 bytes. The list could look like this:
 
-                int newLastIndex = length - 1;
+                    |<---------- data.Length = 8 bytes ---------------------------->|
+                    |<---------- bitLength = 43 bits --------->|                    |
+                    |                                          |                    |
+                    +-------+-------+-------+-------+-------+-------+-------+-------+
+                    |   0   |   1   |   2   |   3   |   4   |   5   |   6   |   7   |
+                    +-------+-------+-------+-------+-------+-------+-------+-------+
 
-                if( value > this.length )
+                    - 6 total bytes are in use - BitsToBytes( 43 ) = 6
+                    - 43 mod 8 = 3, meaning that the last byte is partially used. 3 bits are in use, 5 are not.
+                    - Bytes 0-4 are completely used. 
+                    - Byte 5 is partially used.
+                    - Bytes 6 and 7 are completely unused, and are past the logical end of the list.
+                */
+
+                int newBitLength = value;
+                int newLastIndex = newBitLength - 1;
+
+                if( newBitLength == this.bitLength )
                 {
-                    EnsureCapacity( length - 1 );
+                    return;
+                }
+                else if( newBitLength > this.bitLength )
+                {
+                    EnsureCapacity( newLastIndex );
                 }
                 else
                 {
-                    // We're shrinking the logical size. Keep in mind that we may or may not shrink
-                    // the array.
+                    // - Resize the array if we can give up an entire chunk.
+                    // - Clear bits that are past the logical end of the array. This could be bits in
+                    //   unused bytes, or high bits in a partially used byte.
 
-                    int newArraySize = ArraySize( value );
+                    int newArrayLength = CalcArrayLength( newBitLength );
 
-                    if( newArraySize < this.data.Length )
+                    if( newArrayLength < this.data.Length )
                     {
-
+                        Array.Resize( ref this.data, newArrayLength );
                     }
+
+                    // How many bits are used in the the partially-used block (if one exists).
+                    int partialBits = newBitLength % 8;
+
+                    // The last byte used by the list. May be partially filled, or completely filled.
+                    int lastByte = BitsToBytes( newBitLength ) - 1;
+
+                    // See if we have a partially used byte and clear its high bits if so.
+                    if( partialBits > 0 )
+                    {
+                        // Generate a mask that matches only the bottom partial bits.
+                        byte mask = (byte)(( 1 << partialBits ) - 1);
+
+                        // Zero the high bits using the mask.
+                        this.data[lastByte] &= mask;
+                    }
+
+                    // Clear all bytes after the last byte.
+                    int firstUnused = lastByte + 1;
+                    Array.Clear( this.data, firstUnused, this.data.Length -  firstUnused );
                 }
             }
         }
-
-        public int Count { get; private set; }
-
+        
         public bool this[int index]
         {
             get
@@ -119,26 +181,6 @@ namespace Antiduh.ClassLib
             {
                 Set( index, value );
             }
-        }
-
-        /// <summary>
-        /// Calculates the number of bytes needed to store the given number of bits.
-        /// </summary>
-        /// <param name="numBits"></param>
-        /// <returns></returns>
-        public static int BitsToBytes( int numBits )
-        {
-            if( numBits < 0 )
-            {
-                throw new ArgumentOutOfRangeException( nameof( numBits ), numBits, "Must be zero or greater." );
-            }
-
-            if( numBits == 0 )
-            {
-                return 0;
-            }
-
-            return ( numBits - 1 ) / 8 + 1;
         }
 
         public void Set( int index, bool value )
@@ -209,13 +251,44 @@ namespace Antiduh.ClassLib
 
         private void EnsureCapacity( int requestedIndex )
         {
-            if( requestedIndex >= this.length )
+            int newBitLength = requestedIndex + 1;
+
+            if( newBitLength > this.bitLength )
             {
-                Array.Resize( ref this.data, ArraySize( requestedIndex + 1 ) );
+                int newByteLength = CalcArrayLength( newBitLength );
+
+                // Resize the array if need. May not be needed if there's room in the current chunk.
+                if( newByteLength > this.data.Length )
+                {
+                    Array.Resize( ref this.data, newByteLength );
+                }
+
+                // Save how many bits are in use now.
+                this.bitLength = newBitLength;
             }
         }
 
-        private int ArraySize( int numBits )
+        /// <summary>
+        /// Calculates the number of bytes needed to store the given number of bits.
+        /// </summary>
+        /// <param name="numBits"></param>
+        /// <returns></returns>
+        public static int BitsToBytes( int numBits )
+        {
+            if( numBits < 0 )
+            {
+                throw new ArgumentOutOfRangeException( nameof( numBits ), numBits, "Must be zero or greater." );
+            }
+
+            if( numBits == 0 )
+            {
+                return 0;
+            }
+
+            return ( numBits - 1 ) / 8 + 1;
+        }
+
+        private static int CalcArrayLength( int numBits )
         {
             int numBytes;
             
@@ -224,6 +297,5 @@ namespace Antiduh.ClassLib
 
             return numBytes;
         }
-
     }
 }

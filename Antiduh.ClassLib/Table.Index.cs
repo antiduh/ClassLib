fe7b5db3e9b4fbd1212b78d918436417a48c804a
Kevin Thompson <antiduh@gmail.com>
2020-10-28 01:30:48 -0400

    New Table class, an idea borrowed from SQL - store records in an IList<T>, but also be able to define indexes on those records based on a key selector. Maybe you have a list of people and the city they're born in, and you'd like to define an index that groups people by their cities. The Table class allows you to do this.

---
+++ Antiduh.ClassLib/Table.Index.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Antiduh.ClassLib
+{
+    public partial class Table<TVal> : IList<TVal>
+    {
+        private class TableIndex<TKey> : ITableIndex<TKey, TVal>, IIndexUpdate
+        {
+            private readonly Func<TVal, TKey> keySelector;
+            private readonly Dictionary<TKey, IList<TVal>> map;
+
+            public IEnumerable<TKey> Keys => this.map.Keys;
+
+            public IEnumerable<IList<TVal>> Values => this.map.Values;
+
+            public int Count => throw new NotImplementedException();
+
+            public TableIndex( Func<TVal, TKey> selector )
+            {
+                this.keySelector = selector;
+
+                this.map = new Dictionary<TKey, IList<TVal>>();
+
+            }
+
+            public IList<TVal> this[TKey key] => this.map[key];
+
+            public bool ContainsKey( TKey key )
+            {
+                return this.map.ContainsKey( key );
+            }
+
+            public bool TryGetValue( TKey key, out IList<TVal> value )
+            {
+                return this.map.TryGetValue( key, out value );
+            }
+
+            public IEnumerator<KeyValuePair<TKey, IList<TVal>>> GetEnumerator()
+            {
+                return this.map.GetEnumerator();
+            }
+
+            void IIndexUpdate.Update( TVal newRecord )
+            {
+                TKey key = this.keySelector( newRecord );
+
+                IList<TVal> records;
+
+                if( this.map.TryGetValue( key, out records ) == false )
+                {
+                    records = new List<TVal>();
+                    this.map[key] = records;
+                }
+
+                records.Add( newRecord );
+            }
+
+            void IIndexUpdate.Remove( TVal record )
+            {
+                TKey key = this.keySelector( record );
+
+                this.map.Remove( key );
+            }
+
+            void IIndexUpdate.Clear()
+            {
+                this.map.Clear();
+            }
+
+            IEnumerator IEnumerable.GetEnumerator()
+            {
+                return ( (IEnumerable)this.map ).GetEnumerator();
+            }
+        }
+    }
+
+}
---
+++ Antiduh.ClassLib/Table.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Antiduh.ClassLib
+{
+    public partial class Table<TVal> : IList<TVal>
+    {
+        private readonly List<TVal> list;
+
+        private readonly List<IIndexUpdate> tableIndexes;
+
+        public Table()
+        {
+            this.list = new List<TVal>();
+
+            this.tableIndexes = new List<IIndexUpdate>();
+        }
+
+        public TVal this[int index]
+        {
+            get
+            {
+                return this.list[index];
+            }
+            set
+            {
+                var oldItem = this.list[index];
+
+                foreach( var tableIndex in this.tableIndexes )
+                {
+                    tableIndex.Remove( oldItem );
+                }
+
+                this.list[index] = value;
+
+                foreach( var tableIndex in this.tableIndexes )
+                {
+                    tableIndex.Update( value );
+                }
+            }
+        }
+
+        public int Count => this.list.Count;
+
+        public bool IsReadOnly => false;
+
+        public void Add( TVal item )
+        {
+            this.list.Add( item );
+
+            foreach( var tableIndex in this.tableIndexes )
+            {
+                tableIndex.Update( item );
+            }
+        }
+
+        public ITableIndex<TKey, TVal> CreateIndex<TKey>( Func<TVal, TKey> selector )
+        {
+            var index = new TableIndex<TKey>( selector );
+
+            this.tableIndexes.Add( index );
+
+            return index;
+        }
+
+        public void Clear()
+        {
+            this.list.Clear();
+
+            foreach( var tableIndex in this.tableIndexes )
+            {
+                tableIndex.Clear();
+            }
+        }
+
+        public bool Contains( TVal item )
+        {
+            return this.list.Contains( item );
+        }
+
+        public void CopyTo( TVal[] array, int arrayIndex )
+        {
+            this.list.CopyTo( array, arrayIndex );
+        }
+
+        public IEnumerator<TVal> GetEnumerator()
+        {
+            return this.list.GetEnumerator();
+        }
+
+        public int IndexOf( TVal item )
+        {
+            return this.list.IndexOf( item );
+        }
+
+        public void Insert( int index, TVal item )
+        {
+            this.list.Insert( index, item );
+
+            foreach( var tableIndex in this.tableIndexes )
+            {
+                tableIndex.Update( item );
+            }
+        }
+
+        public bool Remove( TVal item )
+        {
+            foreach( var tableIndex in this.tableIndexes )
+            {
+                tableIndex.Remove( item );
+            }
+
+            return this.list.Remove( item );
+        }
+
+        public void RemoveAt( int index )
+        {
+            TVal item = this.list[index];
+            foreach( var tableIndex in this.tableIndexes )
+            {
+                tableIndex.Remove( item );
+            }
+
+            this.list.RemoveAt( index );
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.list.GetEnumerator();
+        }
+
+        private interface IIndexUpdate
+        {
+            void Update( TVal record );
+
+            void Remove( TVal record );
+
+            void Clear();
+        }
+
+    }
+
+    public interface ITableIndex<TKey, TVal> : IReadOnlyDictionary<TKey, IList<TVal>>
+    {
+    }
+}
